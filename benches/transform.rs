1f09745d50cb38b23e0a4ab2bd8395fe69a0c2d7
Mattia Procopio <promat85@gmail.com>
2022-07-24 21:13:03 +0200

    Add bench for deg to hms transformation

--- benches/transform.rs
+++ benches/transform.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use runiverse::transform::{deg_to_dms, ra_to_deg};
+use runiverse::transform::{deg_to_dms, deg_to_ra, ra_to_deg};
 use runiverse::RightAscension;
 
 fn ra_to_deg_positive_angle(c: &mut Criterion) {
@@ -31,11 +31,19 @@ fn deg_to_positive_dms(c: &mut Criterion) {
     });
 }
 
+fn deg_to_hms(c: &mut Criterion) {
+    let deg = black_box(78.1038);
+    c.bench_function("Transform an angle to a `HoursMinSec` type", |b| {
+        b.iter(|| deg_to_ra(deg))
+    });
+}
+
 criterion_group!(
     benches,
     ra_to_deg_positive_angle,
     ra_to_deg_negative_angle,
     deg_to_negative_dms,
-    deg_to_positive_dms
+    deg_to_positive_dms,
+    deg_to_hms,
 );
 criterion_main!(benches);
